9ae6061995293f3f32bc98fc42f4d98af596f259
timur <t.worck.m@gemail.com>
2023-07-01 00:01:27 +0200

    commit folder HW31 part1

--- src/Model/Emplyee.java
+++ src/Model/Emplyee.java
@@ -1,4 +1,64 @@
 package Model;
 
+import java.util.Objects;
+
 public class Emplyee {
+    protected int id;
+    protected String name;
+    protected int yearOfBirth;
+    protected int experience;
+
+    public Emplyee(int id, String name, int yearOfBirth, int experience) {
+        this.id = id;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.experience = experience;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", name=" + name +
+                ", yearOfBirth=" + yearOfBirth +
+                ", experience=" + experience;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Emplyee)) return false;
+
+        Emplyee emplyee = (Emplyee) o;
+
+        return id == emplyee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
