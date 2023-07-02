e8bc012cb581b2c1e0fbabd78b4eb98c01294e52
timur <t.worck.m@gemail.com>
2023-07-02 22:05:15 +0200

    commit folder HW31 part4

--- src/Test/CompanyImplTest.java
+++ src/Test/CompanyImplTest.java
@@ -24,8 +24,8 @@ class CompanyImplTest {
 
         employees = new Employee[4];
         employees[0] = new Engineer(1,"Eva", 1990, 10, true, 50, 80);
-        employees[1] = new Engineer(2,"John", 2000, 2, false, 30, 50);
-        employees[2] = new Worker(3,"Tom", 1995, 5, true, 30, 100);
+        employees[1] = new Worker(2,"Tom", 1995, 5, false, 30, 100);
+        employees[2] = new Engineer(3,"John", 2000, 2, true, 30, 50);
         employees[3] = new Worker(4,"Bob", 2001, 1, false, 15, 100);
 
         for (int i = 0; i < employees.length; i++) {
@@ -35,18 +35,43 @@ class CompanyImplTest {
 
     @Test
     void hireEmployee() {
+        System.out.println("================ hireEmployee ==================");
+        System.out.println("пытаемся добавить null, и двух сотрудников:");
+        System.out.println("1. Engineer(5,\"Den\", 1998, 4, true, 40, 50)");
+        System.out.println("2. Engineer(6,\"Nick\", 1999, 3, true, 35, 50)");
+        System.out.println("получаем результат:");
+        company.hireEmployee(null);
+        company.hireEmployee(new Engineer(5,"Den", 1998, 4, true, 40, 50));
+        company.hireEmployee(new Engineer(6,"Nick", 1999, 3, true, 35, 50));
+        company.printEmployees();
+        System.out.println("null и 2-й сотрудник не добавились:");
+        System.out.println("- программой предусмотрен запрет на добовление null");
+        System.out.println("- 2 сотрудник не добавился из за переполнения массива (этот массив максимум на 5 сотрудников)");
     }
 
     @Test
     void fireEmployee() {
+        System.out.println("================ fireEmployee ==================");
+        System.out.println("пробуем уволить сотрудников с id: 3 и 5");
+        System.out.println("получаем результат:");
+        company.fireEmployee(3);
+        company.fireEmployee(5);
+        company.printEmployees();
     }
 
     @Test
     void findEmployee() {
+        System.out.println("================ findEmployee ==================");
+        System.out.println("пробуем найти сотрудников с id: 3 и 5");
+        System.out.println("получаем результат:");
+        System.out.println(company.findEmployee(3));
+        System.out.println(company.findEmployee(5));
     }
 
     @Test
     void numberOfEmployees() {
+        System.out.println("================ numberOfEmployees ==================");
+        System.out.println("количество сотрудников - " + company.numberOfEmployees());
     }
 
     @Test
